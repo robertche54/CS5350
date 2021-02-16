a4a8e4c7022cf915ce67f4cbc4be0322f7e196a9
robertche54 <u1119412@utah.edu>
2021-02-16 01:53:45 -0700

    Finished functions for writing to sql database

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,13 +1,36 @@
-import csv
+import sqlite3 as sl
 
-def read():
-    with open ("DecisionTree/test.csv", 'r') as f:
+con = sl.connect(':memory:')
+columns = []
+
+def init_sql(filename):
+    global columns
+    with open("DecisionTree/" + filename, 'r') as f:
+        line = f.readlines()[-1]
+        columns = line.split(',')
+
+    values = ""
+    for column in columns:
+        values += column + " TEXT, "
+    values = values[:-2]
+
+    with con:
+        con.execute(""" 
+            CREATE TABLE data ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            """ + values + " ); ")
+        #print(con.cursor().execute("DESCRIBE data").fetchall())
+
+def read(filename):
+    with open ("DecisionTree/" + filename, 'r') as f:
         for line in f:
             terms = line.strip().split(',')
-            print(line)
+            values = ', '.join(columns)[:-1]
+            params = ['?' for item in terms]
+            con.execute("INSERT INTO data (" + values + ") VALUES (%s);" % ','.join(params), terms)
             pass
 
 def main():
-    read()
+    init_sql("data-desc.txt")
+    read("train.csv")
 
 main()
