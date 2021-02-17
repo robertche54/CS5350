0c31e737115584100d547ed847ff159a46986149
robertche54 <u1119412@utah.edu>
2021-02-17 13:52:02 -0700

    Finished built_tree and learn functions

--- DecisionTree/decisiontree.py
+++ DecisionTree/decisiontree.py
@@ -3,24 +3,32 @@ import math
 from queue import PriorityQueue
 
 con = sl.connect(':memory:')
-columns = []
+root = object()
+attributes = []
 
-class Node:
+class Node():
     thresholds = {}
     pivot = ""
 
+    def __init__(self, pivot):
+        self.pivot = pivot
+
     def evaluate(values):
-        return thresholds[values[pivot]]
+        value = thresholds[values[pivot]]
+        if(isinstance(value, Node)):
+            return value.evaluate(values)
+        else:
+            return value
     pass
 
 def init_sql(filename):
-    global columns
+    global attributes
     with open("DecisionTree/" + filename, 'r') as f:
         line = f.readlines()[-1]
-        columns = line.split(',')
+        attributes = line.split(',')
 
     values = ""
-    for column in columns:
+    for column in attributes:
         values += column + " TEXT, "
     values = values[:-2]
 
@@ -34,32 +42,55 @@ def read(filename):
     with open ("DecisionTree/" + filename, 'r') as f:
         for line in f:
             terms = line.strip().split(',')
-            values = ', '.join(columns)[:-1]
+            values = ', '.join(attributes)[:-1]
             params = ['?' for item in terms]
             con.execute("INSERT INTO data (" + values + ") VALUES (%s)" % ','.join(params), terms)
             pass
 
-def entropy(limit):
-    column = columns[len(columns)-1]
-    i = 0
+def predict(filename):
+    data = []
+    with open ("DecisionTree/" + filename, 'r') as f:
+        for line in f:
+            terms = line.strip().split(',')
+            sample = {}
 
-    values = con.execute("SELECT " + column + ", COUNT(*) as [Count] FROM data " + limit + "GROUP BY " + column + " order by [Count] desc").fetchall()
-    total = con.execute("SELECT COUNT(*) from data " + limit).fetchall()[0][0]
-    for pair in values:
-        ratio = pair[1]/total
-        i -= ratio * math.log(ratio)
-    return i
+            pass
 
-def find_optimal_attribute(columns, limits):
+def make_limit(limits):
     limit = "WHERE"
     if limits:
         for pair in limits:
             # 5 characters are always removed in case limits is empty so 2 spaces are put in front of AND
             limit += " " + pair[0] + "='" + pair[1] + "' AND  "
             # limit = limit[:-5]
+    return limit
 
+def entropy(limit):
+    label = attributes[len(attributes)-1]
+    i = 0
+    values = con.execute("SELECT " + label + ", COUNT(*) as [Count] FROM data " 
+                         + limit + "GROUP BY " + label + " order by [Count] desc").fetchall()
+    total = con.execute("SELECT COUNT(*) from data " + limit).fetchall()[0][0]
+    for pair in values:
+        ratio = pair[1]/total
+        i -= ratio * math.log(ratio)
+    return i
+
+def mode(column, limits):
+    limit = make_limit(limits)[:-5]
+    values = con.execute("SELECT " + column + ", COUNT(*) as [Count] FROM data " 
+                         + limit + "GROUP BY " + column + " order by [Count] desc").fetchall()
+    median = values[0][0]
+    largest = values[0][1]
+    for pair in values:
+        if pair[1] > largest:
+            median = pair[0]
+    return (median, len(values) == 1)
+
+def find_optimal_attribute(columns, limits):
+    limit = make_limit(limits)
     best = PriorityQueue()
-    for i in range(len(columns)-1):
+    for i in range(len(columns)):
         values = con.execute("SELECT " + columns[i] + ", COUNT(*) as [Count] FROM data " 
                              + limit[:-5] + "GROUP BY " + columns[i] + " order by [Count] desc").fetchall()
         total = con.execute("SELECT COUNT(*) from data " + limit[:-5]).fetchall()[0][0]
@@ -70,18 +101,36 @@ def find_optimal_attribute(columns, limits):
 
     return(best.get()[1])
 
-def build_tree(depth):
-    current = find_optimal_attribute(columns, [])
-    newcolumns = columns.copy().remove(current)
-    # make tree based off that attribute
-    # remove that attribute from the attribute pool
-    # for each split in tree
-    # find optimal attribute
-    # limit that split
+def build_tree(current, columns, limits, depth):
+    print(current.pivot)
+    new_depth = depth-1
+    values = con.execute("SELECT DISTINCT " + current.pivot + " FROM data " + make_limit(limits)[:-5]).fetchall()
+    for value in values:
+        new_limits = limits.copy()
+        new_limits.append((current.pivot, value[0]))
+        labels = mode(attributes[len(attributes)-1], new_limits)
+        if new_depth == 0 or labels[1]:
+            current.thresholds[value[0]] = labels[0]
+        else:
+            new_columns = columns.copy()
+            next = find_optimal_attribute(columns, new_limits)
+            new_columns.remove(next)
+            leaf = Node(next)
+            build_tree(leaf, new_columns, new_limits, new_depth)
+            current.thresholds[value[0]] = leaf
+    pass
+
+def learn(max_depth):
+    new_columns = attributes.copy()
+    new_columns.pop()
+    current = find_optimal_attribute(new_columns, [])
+    root = Node(current)
+    new_columns.remove(current)
+    build_tree(root, new_columns, [], max_depth)
 
 def main():
     init_sql("data-desc.txt")
     read("train.csv")
-    build_tree(-1)
+    learn(-1)
 
 main()
