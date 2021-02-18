e5fd816c4d683ab4a3cefd31108ef595367b6b3a
robertche54 <u1119412@utah.edu>
2021-02-17 18:00:59 -0700

    Fixed class variables being shared by moving them to the constructor

--- DecisionTree/decisiontree.py
+++ DecisionTree/decisiontree.py
@@ -7,18 +7,21 @@ root = object()
 attributes = []
 
 class Node():
-    thresholds = {}
-    pivot = ""
 
     def __init__(self, pivot):
+        self.thresholds = {}
         self.pivot = pivot
 
-    def evaluate(values):
-        value = thresholds[values[pivot]]
+    def evaluate(self, values):
+        value = self.thresholds[values[self.pivot]]
         if(isinstance(value, Node)):
             return value.evaluate(values)
         else:
             return value
+
+    def add_node(self, key, node):
+        self.thresholds[key] = node
+
     pass
 
 def init_sql(filename):
@@ -26,6 +29,7 @@ def init_sql(filename):
     with open("DecisionTree/" + filename, 'r') as f:
         line = f.readlines()[-1]
         attributes = line.split(',')
+        pass
 
     values = ""
     for column in attributes:
@@ -53,8 +57,15 @@ def predict(filename):
         for line in f:
             terms = line.strip().split(',')
             sample = {}
-
+            for i in range(len(terms)):
+                sample[attributes[i]] = terms[i]
+            data.append(sample)
             pass
+        pass
+
+    for sample in data:
+        print(sample)
+        print(root.evaluate(sample))
 
 def make_limit(limits):
     limit = "WHERE"
@@ -80,12 +91,12 @@ def mode(column, limits):
     limit = make_limit(limits)[:-5]
     values = con.execute("SELECT " + column + ", COUNT(*) as [Count] FROM data " 
                          + limit + "GROUP BY " + column + " order by [Count] desc").fetchall()
-    median = values[0][0]
+    mode = values[0][0]
     largest = values[0][1]
     for pair in values:
         if pair[1] > largest:
-            median = pair[0]
-    return (median, len(values) == 1)
+            mode = pair[0]
+    return (mode, len(values) == 1)
 
 def find_optimal_attribute(columns, limits):
     limit = make_limit(limits)
@@ -98,39 +109,52 @@ def find_optimal_attribute(columns, limits):
         for pair in values:
             loss += (pair[1]/total) * entropy(limit + " " + columns[i] + "='" + pair[0] + "' ")
         best.put((loss, columns[i]))
+        pass
 
     return(best.get()[1])
 
 def build_tree(current, columns, limits, depth):
-    print(current.pivot)
-    new_depth = depth-1
+    depth -= 1
     values = con.execute("SELECT DISTINCT " + current.pivot + " FROM data " + make_limit(limits)[:-5]).fetchall()
     for value in values:
         new_limits = limits.copy()
         new_limits.append((current.pivot, value[0]))
         labels = mode(attributes[len(attributes)-1], new_limits)
-        if new_depth == 0 or labels[1]:
-            current.thresholds[value[0]] = labels[0]
+
+        if depth == 0 or labels[1]:
+            if(value[0] == 'big' and current.pivot == "persons"):
+                print(current.pivot)
+            #current.thresholds[value[0]] = labels[0]
+            current.add_node(value[0], labels[0])
         else:
             new_columns = columns.copy()
-            next = find_optimal_attribute(columns, new_limits)
+            next = find_optimal_attribute(new_columns, new_limits)
             new_columns.remove(next)
             leaf = Node(next)
-            build_tree(leaf, new_columns, new_limits, new_depth)
-            current.thresholds[value[0]] = leaf
-    pass
+            build_tree(leaf, new_columns, new_limits, depth)
+            #current.thresholds[value[0]] = leaf
+            current.add_node(value[0], leaf)
+
+        for n in nodelist:
+            if not n.valid():
+                print("INVALID")
+        pass
 
 def learn(max_depth):
+    global root
     new_columns = attributes.copy()
     new_columns.pop()
     current = find_optimal_attribute(new_columns, [])
-    root = Node(current)
     new_columns.remove(current)
+    root = Node(current)
     build_tree(root, new_columns, [], max_depth)
 
 def main():
     init_sql("data-desc.txt")
     read("train.csv")
     learn(-1)
+    predict("test.csv")
 
 main()
+    
+    
