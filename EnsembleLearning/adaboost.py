4ed683ffd3ef5d1f7195b9e34f31ab28d41efacb
robertche54 <u1119412@utah.edu>
2021-03-08 20:20:50 -0800

    Continue working on debugging

--- EnsembleLearning/adaboost.py
+++ EnsembleLearning/adaboost.py
@@ -5,51 +5,71 @@ import re
 import random
 
 con = sl.connect(':memory:')
+# The name of each column
 attributes = []
+# The columns that need to be split with median value
 to_process = []
-stumps = []
-weights = []
+# List of medians for columns that need to be processed
 medians = {}
+# Most frequent value in each column
 modes = {}
 
+# Holds the list of stumps
+stumps = []
+# List of weights that uses sql id as index
+weights = []
+
+# Returns if string s is an integer
 def IsInt(s):
     return re.match(r"[-+]?\d+(\.0*)?$", s) is not None
 
+# Stump class that is a depth 1 tree
 class Stump():
     
+    # Pivot is the column
+    # Classifier is the label that the column is split to find
     def __init__(self, pivot, classifier):
         self.thresholds = {}
         self.pivot = pivot
         self.classifier = classifier
         self.total_error = 0
         
+        # Gets all attributes for the column
         values = con.execute("SELECT DISTINCT " + pivot + " FROM data").fetchall()
         label = attributes[len(attributes)-1]
+
+        # For every attribute
         for value in values:
 
             # This is unbelievably stupid
             limit = "WHERE " + pivot + " = '" + value[0] + "'"
             yes_limit = " AND " + label + " = '" + classifier + "'"
             no_limit = " AND " + label + " != '" + classifier + "'"
-            #yes = 0
-            #no = 0
-            #for pair in con.execute("SELECT id FROM data " + limit + yes_limit).fetchall():
-            #    yes += weights[int(pair[0])-1]
-            #for pair in con.execute("SELECT id FROM data " + limit + no_limit).fetchall():
-            #    no += weights[int(pair[0])-1]
 
-            yes = con.execute("SELECT COUNT(*) FROM data " + limit + yes_limit).fetchall()[0][0]
-            no = con.execute("SELECT COUNT(*) FROM data " + limit + no_limit).fetchall()[0][0]
+
+            count = {"yes" : 0, "no" : 0}
+            for pair in con.execute("SELECT id FROM data " + limit + yes_limit).fetchall():
+                count["yes"] += weights[int(pair[0])-1]
+            for pair in con.execute("SELECT id FROM data " + limit + no_limit).fetchall():
+                count["no"] += weights[int(pair[0])-1]
+
+            # Finds number of rows where the (pivot) column is equal to the attribute for label
+            # yes = con.execute("SELECT COUNT(*) FROM data " + limit + yes_limit).fetchall()[0][0]
+            # no = con.execute("SELECT COUNT(*) FROM data " + limit + no_limit).fetchall()[0][0]
 
             # print(value[0] + " " + str(yes) + " " + str(no))
 
-            if yes >= no:
+            # If the leaf node predicts the label
+            if count["yes"] >= count["no"]:
                self.thresholds[value[0]] = 1
-               self.total_error += self.weighted_error(limit + no_limit)
+               # The error will be the rows that don't return the prediction, aka opposite
+               self.total_error += count["no"]
+               #self.total_error += self.weighted_error(limit + no_limit)
                # print(self.pivot + " error: " + limit + " = " + str(self.weighted_error(limit + no_limit)))
             else:
                self.thresholds[value[0]] = -1
-               self.total_error += self.weighted_error(limit + yes_limit)
+               self.total_error += count["yes"]
+               #self.total_error += self.weighted_error(limit + yes_limit)
                # print(self.pivot + " error: " + limit + " = " + str(self.weighted_error(limit + no_limit)))
             pass
 
@@ -63,12 +83,13 @@ class Stump():
         self.weight = 0.5 * math.log((1 - self.total_error)/self.total_error)
         pass
 
-    def weighted_error(self, limit):
-        error = 0
-        ids = con.execute("SELECT id FROM data " + limit).fetchall()
-        for id in ids:
-            error += weights[int(id[0])-1]
-        return error
+    #def weighted_error(self, limit):
+    #    error = 0
+    #    ids = con.execute("SELECT id FROM data " + limit).fetchall()
+    #    for id in ids:
+    #        #print(limit + "    " + str(id) + " " + str(weights[int(id[0])-1]))
+    #        error += weights[int(id[0])-1]
+    #    return error
 
     def evaluate(self, values):
         return self.weight * self.thresholds[values[self.pivot]]
@@ -208,6 +229,7 @@ def total_weights():
         total += weight
     return total
 
+# This finds the total entropy for a specific column
 def weighted_entropy(column, classifier):
     label = attributes[len(attributes)-1]
     total = total_weights()
@@ -216,22 +238,30 @@ def weighted_entropy(column, classifier):
     values = con.execute("SELECT DISTINCT " + column + " FROM data").fetchall()
     for value in values:
         counts = {"total" : 0, "yes" : 0, "no" : 0}
+        # Finds ever row where the column = the attribute
+		# row[0] is the id, which is used for the index of the weight list
+		# row[1] is the label which is either yes or no
         data = con.execute("SELECT id, " + label + " FROM data WHERE " + column + " = '" + value[0] + "'").fetchall()
         for row in data:
+            # total additive weights
             counts["total"] += weights[row[0]-1]
             if row[1] == classifier:
+                # amount of label = yes
                 counts["yes"] += weights[row[0]-1]
             else:
+                # amount of label = no
                 counts["no"] += weights[row[0]-1]
             pass
         yes = counts["yes"]/counts["total"]
         no = counts["no"]/counts["total"]
+        # entropy = (Sv/1) * (+p * ln(+p) + -p ln(-p)
         entropy -= (counts["total"]/total) * (yes * math.log(yes) + no * math.log(no))
         pass
     # print(column + " " + str(entropy))
     return entropy
     pass
 
+# finds the best stump by highest weight
 def next_best_stump(classifier):
     best_stump = Stump(attributes[0], classifier)
     for i in range(len(attributes)-1):
@@ -243,7 +273,8 @@ def next_best_stump(classifier):
         pass
     return best_stump
 
-def next_best_stump_2(classifier):
+# finds the best stump based on fractional, weighted information gain (entropy)
+def best_gain_stump(classifier):
     best_attribute = attributes[0]
     entropy = weighted_entropy(best_attribute, classifier)
     for i in range(len(attributes)-1):
@@ -255,32 +286,36 @@ def next_best_stump_2(classifier):
     # print(best_stump.pivot + " " + str(best_stump.total_error))
     return Stump(best_attribute, classifier)
 
-# why not
-def bobo_sort(classifier):
-    new_attributes = attributes.copy()
-    # print(attributes[len(attributes)-1])
-    new_attributes.remove(attributes[len(attributes)-1])
-    rand = random.randint(0, len(new_attributes)-1)
-    obviously_the_best_stump = Stump(new_attributes[rand], classifier)
-    new_attributes.remove(new_attributes[rand])
-    while len(new_attributes) > 0 and (obviously_the_best_stump.weight < 0.6):
-        rand = random.randint(0, len(new_attributes)-1)
-        obviously_the_best_stump = Stump(new_attributes[rand], classifier)
-        new_attributes.remove(new_attributes[rand])
-    return obviously_the_best_stump
+# for testing purposes
+#def bobo_sort(classifier):
+#    new_attributes = attributes.copy()
+#    # print(attributes[len(attributes)-1])
+#    new_attributes.remove(attributes[len(attributes)-1])
+#    rand = random.randint(0, len(new_attributes)-1)
+#    obviously_the_best_stump = Stump(new_attributes[rand], classifier)
+#    new_attributes.remove(new_attributes[rand])
+#    while len(new_attributes) > 0 and (obviously_the_best_stump.weight < 0.6):
+#        rand = random.randint(0, len(new_attributes)-1)
+#        obviously_the_best_stump = Stump(new_attributes[rand], classifier)
+#        new_attributes.remove(new_attributes[rand])
+#    return obviously_the_best_stump
 
 def calculate_new_weights(stump, classifier):
     label = attributes[len(attributes)-1]
     total = 0
     values = con.execute("SELECT id, " + stump.pivot + ", " + label + " FROM data").fetchall()
     for value in values:
+        # Sample is correctly identified, (yi = h(xi))
         if (stump.thresholds[value[1]] == 1 and value[2] == classifier) or (stump.thresholds[value[1]] == -1 and value[2] != classifier):
+            # multiply dt with e^-a
             weights[value[0]-1] *= math.exp(-stump.weight)
         else:
+            # multiply dt with e^a
             weights[value[0]-1] *= math.exp(stump.weight)
         total += weights[value[0]-1]
         pass
 
+    # normalize all weights to 1
     for i in range(len(weights)):
         weights[i] /= total
 
@@ -292,7 +327,13 @@ def calculate_new_weights(stump, classifier):
 
 def learn(max_depth, classifier):
     global stumps
+    global weights
+
+    # Clears stumps and weights
     stumps = []
+    weights = []
+
+    # Update each weight to be 1/# of rows
     total = con.execute("SELECT COUNT(*) FROM data").fetchall()[0][0]
     for i in range(total):
         weights.append(float(1/total))
@@ -301,11 +342,12 @@ def learn(max_depth, classifier):
     #print("weights before : " + str(weights))
 
     for i in range(max_depth):
-        stump = bobo_sort(classifier)
+        stump = best_gain_stump(classifier)
         #print("median chosen for patient weight: " + medians[2])
         #print("stump chosen: " + stump.pivot + " " + str(stump.weight) + " " + str(stump.total_error))
         calculate_new_weights(stump, classifier)
         #print("weights after : " + str(weights))
+        #print(stump.pivot + " " + str(stump.total_error))
         stumps.append(stump)
     pass
 
@@ -315,15 +357,15 @@ def main():
     read("train.csv")
     post_process()
 
+    #learn(10 , "yes")
+    #print(predict("dummy_test.csv"))
+        
     for i in range(1, 501):
         learn(i , "yes")
-        print(predict("train.csv"))
+        print(str(i) + ", " + str(predict("train.csv")) + ", " + str(predict("test.csv")))
 
-    #    learn(i , "yes")
-    #    print(str(i) + ", " + str(predict("train.csv")) + ", " + str(predict("test.csv")))
-
-    #for stump in stumps:
-    #    print(stump.pivot + " " + str(stump.weight) + " " + str(stump.total_error))
-    #pass
+        #for stump in stumps:
+        #    print(stump.pivot + " " + str(stump.weight) + " " + str(stump.total_error))
+        #pass
 
 main()
--- LinearRegression/linearregression.py
+++ LinearRegression/linearregression.py
@@ -1,2 +1,23 @@
-import statistics
-import math
+data = []
+
+def read(filename):
+    with open ("EnsembleLearning/" + filename, 'r') as f:
+        for line in f:
+            terms = line.split(',')
+            row = []
+            for term in terms:
+                row.append(float(term))
+                pass
+            pass
+        pass
+    pass
+
+def learn():
+    
+
+def main():
+    read("train.csv")
+
+    pass
+
+main()
