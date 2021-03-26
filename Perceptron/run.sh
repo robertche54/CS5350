1c21ab5658816663fe7114ab1b1d7a8575ff901a
robertche54 <u1119412@utah.edu>
2021-03-25 19:49:48 -0700

    Added run.sh to Perceptron

---
+++ Perceptron/run.sh
@@ -0,0 +1,3 @@
+py perceptron.py train.csv test.csv stochastic_descent 10 0.01
+py perceptron.py train.csv test.csv stochastic_descent 10 0.01
+py perceptron.py train.csv test.csv solve_math 10 0.01
